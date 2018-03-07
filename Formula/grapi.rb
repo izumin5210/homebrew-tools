2bb54d78e9477fa05bca6ea6cf418962a9b2176d
izumin5210 <m@izum.in>
2018-03-07 17:49:12 +0900

    Bump grapi 0.1.1

--- Formula/grapi.rb
+++ Formula/grapi.rb
@@ -1,5 +1,5 @@
 class Grapi < Formula
-  VERSION = "0.1.0".freeze
+  VERSION = "0.1.1".freeze
   NAME = "grapi".freeze
   REPO = "izumin5210/#{NAME}".freeze
   ARCH = "darwin_amd64".freeze
@@ -8,7 +8,7 @@ class Grapi < Formula
   homepage "https://github.com/#{REPO}"
   url "https://github.com/#{REPO}/releases/download/v#{VERSION}/#{NAME}_#{VERSION}_#{ARCH}.tar.gz"
   version VERSION
-  sha256 "948a28648ed19640c57261ffafcdbb48c93b520142d7244bdf07da632493111b"
+  sha256 "1a83a152a5e11b753d71709aa8134c7d0c439d9d16542af384b713e6aa6e51e1"
 
   def install
     bin.install "#{NAME}_#{VERSION}_#{ARCH}/#{NAME}"
