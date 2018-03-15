70b477414079d5d511adc8d966c211ebcf0b9ed8
izumin5210 <m@izum.in>
2018-03-15 11:33:28 +0900

    Bump grapi v0.1.3

--- Formula/grapi.rb
+++ Formula/grapi.rb
@@ -1,17 +1,19 @@
 class Grapi < Formula
-  VERSION = "0.1.2".freeze
+  VERSION = "0.1.3".freeze
   NAME = "grapi".freeze
   REPO = "izumin5210/#{NAME}".freeze
   ARCH = "darwin_amd64".freeze
+  BIN = "#{NAME}_#{ARCH}".freeze
 
   desc "JSON API framework implemented with gRPC and Gateway"
   homepage "https://github.com/#{REPO}"
-  url "https://github.com/#{REPO}/releases/download/v#{VERSION}/#{NAME}_#{VERSION}_#{ARCH}.tar.gz"
+  url "https://github.com/#{REPO}/releases/download/v#{VERSION}/#{BIN}"
   version VERSION
-  sha256 "a4129fb993fa321ecd74cfbf66ff4348aee107f34e3c182ee4721b8104451e1a"
+  sha256 "133072f0063938c9977c436dd064eb6a6cfaa99e98297fd69dddd0421a9a16c6"
 
   def install
-    bin.install "#{NAME}_#{VERSION}_#{ARCH}/#{NAME}"
+    mv BIN, NAME
+    bin.install NAME
   end
 
   test do
