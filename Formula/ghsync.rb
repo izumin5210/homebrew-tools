c61abafd762aa8a3b0fd09f1fc5316dcaf6b5f4d
izumin5210 <m@izum.in>
2019-07-08 17:39:08 +0900

    Bump izumin5210/ghsync formula

--- Formula/ghsync.rb
+++ Formula/ghsync.rb
@@ -1,8 +1,8 @@
 class Ghsync < Formula
-  version '0.0.1'
+  version '0.0.4'
   homepage 'https://github.com/izumin5210/ghsync'
-  url "https://github.com/izumin5210/ghsync/releases/download/v0.0.1/ghsync_darwin_amd64.zip"
-  sha256 'c92526b29a31b1b009fc5677b333e4f87e9a96d5f8606b9cfd6ac577ae8d4b87'
+  url "https://github.com/izumin5210/ghsync/releases/download/v0.0.4/ghsync_darwin_amd64.zip"
+  sha256 '2f5113b359b5691910d0d45bee53c3e914eab4a99608df58ddf169dbaefb6397'
   head 'https://github.com/izumin5210/ghsync.git'
 
   head do
