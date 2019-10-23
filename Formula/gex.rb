b0cb4cdce6cb9148462deded7242ff9bbff03b5b
ossrabbit <43696030+ossrabbit@users.noreply.github.com>
2019-10-23 22:16:49 +0900

    Bump izumin5210/gex formula

--- Formula/gex.rb
+++ Formula/gex.rb
@@ -1,8 +1,8 @@
 class Gex < Formula
-  version '0.5.1'
+  version '0.6.0'
   homepage 'https://github.com/izumin5210/gex'
-  url "https://github.com/izumin5210/gex/releases/download/v0.5.1/gex_darwin_amd64.zip"
-  sha256 '82985c28d78d41092bc1a1994b441175e34c622e6434c695700faa655e006653'
+  url "https://github.com/izumin5210/gex/releases/download/v0.6.0/gex_darwin_amd64.zip"
+  sha256 '5b10c00eb5bf92dedb579058a273fac3cc7ab10e12fdd9077dafa6196318938c'
   head 'https://github.com/izumin5210/gex.git'
 
   head do
